67aec9355a868500d94d26291d1637ab803ecd49
Yahya Muhammad Mukhlis <yahya.mukhlis@gmail.com>
2016-04-13 13:55:37 +0700

    - add transformations to square image

--- app/src/main/java/com/reanstudio/imagefilter/OpenGLRenderer.java
+++ app/src/main/java/com/reanstudio/imagefilter/OpenGLRenderer.java
@@ -13,6 +13,8 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
 
     private Square square;
 
+    private float angle;
+
     public OpenGLRenderer() {
         this.square = new Square();
     }
@@ -57,8 +59,49 @@ public class OpenGLRenderer implements GLSurfaceView.Renderer {
         // Replace the current matrix with the identity matrix
         gl.glLoadIdentity();
         // Translates 4 units into the screen.
-        gl.glTranslatef(0, 0, -4);
-        // Draw our square.
+        gl.glTranslatef(0, 0, -10);
+
+        // Square A
+        // Save the current matrix
+        gl.glPushMatrix();
+        // Rotate square A counter-clockwise
+        gl.glRotatef(angle, 0, 0, 1);
+        // Draw square A.
         square.draw(gl);
+        // Restore the last matrix
+        gl.glPopMatrix();
+
+        // Square B
+        // Save the current matrix
+        gl.glPushMatrix();
+        // Rotate square B before moving it, making it rotate around A
+        gl.glRotatef(-angle, 0, 0, 1);
+        // Move square B
+        gl.glTranslatef(2, 0, 0);
+        // Scale it to 50% of square A
+        gl.glScalef(.5f, .5f, .5f);
+        // Draw square B.
+        square.draw(gl);
+
+        // Square C
+        // Save the current matrix
+        gl.glPushMatrix();
+        // Make the rotation around B
+        gl.glRotatef(-angle, 0, 0, 1);
+        gl.glTranslatef(2, 0, 0);
+        // Scale it to 50% of square A
+        gl.glScalef(.5f, .5f, .5f);
+        // Rotate around it's own center
+        gl.glRotatef(angle * 10, 0, 0, 1);
+        // Draw square C.
+        square.draw(gl);
+
+        // Restore to the matrix as it was before C
+        gl.glPopMatrix();
+        // Restore to the matrix as it was before B
+        gl.glPopMatrix();
+
+        // Increase the angle
+        angle++;
     }
 }
